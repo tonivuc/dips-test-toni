2eee913a994e5bbe72d5cc4b1bb49d6cfa4a2701
Toni Vucic <tonivuc@gmail.com>
2019-09-13 13:44:43 +0200

    Replaced MainActivity with the tester activity

--- android_cloud_test/android_cloud_test/android_cloud_test.Android/MainActivity.cs
+++ android_cloud_test/android_cloud_test/android_cloud_test.Android/MainActivity.cs
@@ -1,36 +1,4 @@
 ﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-
-namespace android_cloud_test.Droid
-{
-    [Activity(Label = "android_cloud_test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-
-            AppCenter.Start("4f1bd9e8-5b73-44d6-b2a9-7d3955eb3649",
-                   typeof(Analytics), typeof(Crashes));
-        }
-    }
-}
-
-using System;
 using System.Reflection;
 using Android.App;
 using Android.Content;
@@ -42,7 +10,7 @@ using Android.OS;
 using Xunit.Sdk;
 using Xunit.Runners.UI;
 
-namespace $rootnamespace$
+namespace android_cloud_test.Droid
 {
     [Activity(Label = "xUnit Android Runner", MainLauncher = true, Theme= "@android:style/Theme.Material.Light")]
     public class MainActivity : RunnerActivity
@@ -51,12 +19,12 @@ namespace $rootnamespace$
         protected override void OnCreate(Bundle bundle)
         {
             // tests can be inside the main assembly
-            AddTestAssembly(Assembly.GetExecutingAssembly());
+            //AddTestAssembly(Assembly.GetExecutingAssembly());
 
             AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
             // or in any reference assemblies			
 
-            //AddTestAssembly(typeof(PortableTests).Assembly);
+            //AddTestAssembly(typeof(NamesTest).Assembly);
             // or in any assembly that you load (since JIT is available)
 
 #if false
