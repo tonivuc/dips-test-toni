d3694b47e2d20f7e23e4775a637496d37550d062
Toni Vucic <tonivuc@gmail.com>
2019-09-13 11:21:45 +0200

    Added test project and some basic code to test

---
+++ android_cloud_test/android_cloud_test/android_cloud_test/Logic/Names.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace android_cloud_test.Logic
+{
+    class Names
+    {
+        List<String> names;
+
+        public Names(List<String> names)
+        {
+            this.names = names;
+        }
+
+        public Names()
+        {
+            names = new List<String>();
+        }
+
+        public String AddName(String name)
+        {
+            names.Add(name);
+            return names[names.Count - 1];
+        }
+    }
+}
